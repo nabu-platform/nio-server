39a989937b047a8a2a1f5f0ade7f82b5b683a42e
alex <alex@nabu.be>
2015-11-08 17:36:19 +0100

    Added max client connections acceptor

---
+++ src/main/java/be/nabu/libs/nio/impl/MaxClientConnectionsAcceptor.java
@@ -0,0 +1,33 @@
+package be.nabu.libs.nio.impl;
+
+import java.net.InetAddress;
+import java.nio.channels.SocketChannel;
+
+import be.nabu.libs.nio.api.ConnectionAcceptor;
+import be.nabu.libs.nio.api.NIOServer;
+import be.nabu.libs.nio.api.Pipeline;
+
+public class MaxClientConnectionsAcceptor implements ConnectionAcceptor {
+
+	private int amount;
+
+	public MaxClientConnectionsAcceptor(int amount) {
+		this.amount = amount;
+	}
+	
+	@Override
+	public boolean accept(NIOServer server, SocketChannel newConnection) {
+		int current = 0;
+		InetAddress inetAddress = newConnection.socket().getInetAddress();
+		if (inetAddress == null) {
+			return false;
+		}
+		for (Pipeline pipeline : server.getPipelines()) {
+			if (pipeline.getSourceContext().getSocket().getInetAddress() != null && pipeline.getSourceContext().getSocket().getInetAddress().getHostAddress().equals(inetAddress.getHostAddress())) {
+				current++;
+			}
+		}
+		return current < amount;
+	}
+
+}
